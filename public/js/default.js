263eea9eee36e708444a075c35419cfddd268d45
phoenixcqcq <phoenixcqcq@seznam.cz>
2013-09-19 22:19:58 +0200

    js send click

--- public/js/default.js
+++ public/js/default.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
 		'titlePosition' 	: 'over'
 	});
 
+	$(".send").click(function() {
+		var id = $(this).attr('id').substring(5);
+		$("#popUp").html(
+			"<h3>Poslat obrázek</h3>" +
+				"<form action='prace-s-obrazkem/poslat/"+id+"' method='post'>" +
+				"Email: <input type='text' name='email' id='email' />" +
+				"<input type='submit' value='Poslat'>" +
+			"</form>"
+		)
+		$("#sendPopUp").show();
+		$("#popUp").show();
+	});
+
 });
