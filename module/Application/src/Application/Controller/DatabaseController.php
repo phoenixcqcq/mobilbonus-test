e2e523fe86761e926d6dfc1dfef1e771acb5d32e
phoenixcqcq <phoenixcqcq@seznam.cz>
2013-09-20 00:30:45 +0200

    User validator/filter

--- module/Application/src/Application/Controller/DatabaseController.php
+++ module/Application/src/Application/Controller/DatabaseController.php
@@ -14,40 +14,55 @@ use Zend\View\Model\ViewModel;
 use Application\Model\User;
 use Application\Form\UserForm;
 use Application\Model\UserFormModel;
+use Zend\Filter\StaticFilter;
 
 class DatabaseController extends AbstractActionController
 {
     public function indexAction()
     {
         $form = new UserForm();
+        $warning = null;
+        if (isset($_SESSION['warning'])) {
+            $warning = $_SESSION['warning'];
+            unset($_SESSION['warning']);
+        }
         return new ViewModel(array(
                                   'userForm' => $form,
-                                  'users'    => $this->getAllUsers()
+                                  'users'    => $this->getAllUsers(),
+                                  'warning'  => $warning
                              ));
     }
 
     public function ulozitAction()
     {
-        // Get the database adapter
-        $sm = $this->getServiceLocator();
-        $db = $sm->get('db');
+        if($this->validate()){
+            // Get the database adapter
+            $sm = $this->getServiceLocator();
+            $db = $sm->get('db');
 
-        if ($this->getRequest()->isPost()) {
-            $post = $this->getRequest()->getPost();
-            $id = $post['id'];
-            $firstname = $post['firstname'];
-            $lastname = $post['lastname'];
-            $street = $post['street'];
-            $town = $post['town'];
+            if ($this->getRequest()->isPost()) {
+                $post = $this->getRequest()->getPost();
+                $id = $post['id'];
 
-            $userModel = new User($db);
+                $filter = new \Zend\Filter\FilterChain();
+                $filter->attach(new \Zend\Filter\StripTags());
+                $filter->attach(new \Zend\Filter\StringTrim());
 
-            if ($id) {
-                $userModel->updateUser($id, $firstname, $lastname, $street, $town);
-            } else {
-                $userModel->addUser($firstname, $lastname);
+                $firstname = $filter->filter($post['firstname']);
+                $lastname = $filter->filter($post['lastname']);
+                $street = $filter->filter($post['street']);
+                $town = $filter->filter($post['town']);
+                
+                $userModel = new User($db);
+
+                if ($id) {
+                    $userModel->updateUser($id, $firstname, $lastname, $street, $town);
+                } else {
+                    $userModel->addUser($firstname, $lastname);
+                }
             }
         }
+
         $this->redirect()->toUrl($this->getRequest()->getBasePath().'/databaze');
     }
 
@@ -71,6 +86,34 @@ class DatabaseController extends AbstractActionController
         $view = new ViewModel(array('userForm' => $form, 'user' => $user, 'users' => $this->getAllUsers()));
         $view->setTemplate('application/database/index');
         return $view;
+    }
+
+    private function validate()
+    {
+        if ($this->getRequest()->isPost()) {
+            $post = $this->getRequest()->getPost();
+
+            $filter = new \Zend\Filter\FilterChain();
+            $filter->attach(new \Zend\Filter\StripTags());
+            $filter->attach(new \Zend\Filter\StringTrim());
+
+            $firstname = $filter->filter($post['firstname']);
+            $lastname = $filter->filter($post['lastname']);
+
+            $notEmptyValidator = new \Zend\Validator\NotEmpty();
+
+            if (!$notEmptyValidator->isValid($firstname)) {
+                $warning[] = "Jméno nebylo vyplněno.";
+            }
+            if (!$notEmptyValidator->isValid($lastname)) {
+                $warning[] = "Příjmení nebylo vyplněno.";
+            }
+            if(!empty($warning)){
+                $_SESSION['warning'] = implode(" ", $warning);
+                return false;
+            }
+        }
+        return true;
     }
 
     private function getAllUsers()
